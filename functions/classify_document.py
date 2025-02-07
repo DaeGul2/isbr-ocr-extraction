7470ca0ddd79a1ee39ec32484fcb0710e560afc4
민태희 <122341071+DaeGul2@users.noreply.github.com>
2025-02-07 15:00:38 +0900

    dd
    dd

--- functions/classify_document.py
+++ functions/classify_document.py
@@ -4,11 +4,11 @@ def classify_document(text):
     """
     if "주민등록표" in text:
         return "등본" if "초본" not in text else "초본"
-    if "건강보험자격득실 확인서" in text:
-        return "건강보험자격득실_건보" if "G" in text else "건강보험자격득실_정부24"
-    if "국민연금가입자증명서" in text:
-        return "국민연금가입자증명_국민연금" if "국민연금" in text else "국민연금가입자증명_정부24"
-    if "TOEIC" in text or "TEST TAKER" in text:
+    if "건강보험자격득실" in text:
+        return "건강보험자격득실_건보" if ("발급번호" in text or "확인청구" in text) else "건강보험자격득실_정부24"
+    if "국민연금가입자가입" in text:
+        return "국민연금가입자증명_국민연금" if "발급번호" in text else "국민연금가입자증명_정부24"
+    if "TOEIC" in text and "TEST TAKER" in text:
         return "토익"
     if "TOEFL" in text or "SPEAKING" in text:
         return "토스"
